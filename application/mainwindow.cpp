a978dd651e973b0fda5661accef32b45fc7a5e0d
GogoLike <iekalayda@gmail.com>
2024-10-07 17:29:35 +0300

    Add CppCheck and Clang-Format. Add .vscode to .gitignore.

--- application/mainwindow.cpp
+++ application/mainwindow.cpp
@@ -1,13 +1,23 @@
 #include "mainwindow.h"
+
+//! Qt specific includes
+// cppcheck-suppress missingInclude
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget* parent)
+  : QMainWindow(parent)
+  , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
+
+  if (false) {
+    int a = 0;
+  } else {
+    int a = 3;
+  }
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+  delete ui;
 }
-
--- application/mainwindow.h
+++ application/mainwindow.h
@@ -1,19 +1,25 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+//! Qt specific includes
+// cppcheck-suppress missingIncludeSystem
 #include <QMainWindow>
 
+//! Qt specific macro
+// cppcheck-suppress unknownMacro
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  explicit MainWindow(QWidget* parent = nullptr);
+  ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
+  Ui::MainWindow* ui;
 };
 #endif // MAINWINDOW_H
